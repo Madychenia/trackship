64607283c596eef9021d9351fa085b144ea18ef9
Madychenia <alex.madyka@gmail.com>
2026-03-30 18:54:44 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="TrackShip App", page_icon="📦")
+
+st.title("📦 TrackShip: Система отслеживания")
+st.info("Приложение запущено! Жду описание функционала.")
+
+# Здесь будет основной код
